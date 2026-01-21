d29d9874d9ea0f3eaa778cc8b3baf8cc8f142362
Your Name <dilliram2003adhikari@gmail.com>
2026-01-21 18:27:32 +0530

    navbar-1

--- apps/user/component/navbar.tsx
+++ apps/user/component/navbar.tsx
@@ -1,13 +1,17 @@
+import { IconButton } from "@repo/ui/iconButton";
+import { MessagesSquare } from "lucide-react";
 
 
 export function Navbar (){
     return <div className="flex justify-between items-center px-24 py-3 ">
-        <div className="text-">
+        <div className="text-sky-500 font-bold text-2xl">
             MarketValley
         </div>
 
-        <div className="flex">
-            <div>messageIcon</div>
+        <div className="flex ">
+            <div>
+                <IconButton Icon={<MessagesSquare />} size={45}/>
+            </div>
             <div>profile</div>
         </div>
     </div>
